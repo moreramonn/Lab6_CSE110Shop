00c7d40cedbed8b3cf3370fb56497d4546c25590
Mon Morera <mmorera@ucsd.edu>
2021-02-09 18:49:57 -0800

    test part1

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,12 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  let storage = window.localStorage;
+  let productList = document.getElementById('product-list');
+
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      storage.setItem('products', JSON.stringify(data));
+    });
 });
